5f319605a3239e4b5538192e73e49686eb35847c
majandy <andy.frimpong@st.vvu.edu.gh>
2017-04-11 13:04:13 -0700

    task app
    i have home page

---
+++ task_app-master/resources/views/welcome.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.parent')
+<!--Content to serve-->
+@section('killer')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Task App</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+   <!-- <p>Welcome to our task app!!</p>-->
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      
+          <div class="jumbotron">
+             <h1>Welcome to Task App!</h1>
+              <p class="lead">Thank you so much for visiting.Sign Up to Use My Task App. Please Sign Up!</p>
+              <p><a class="btn btn-primary btn-lg" href="#" role="button">LogIn</a></p>
+              <p><a class="btn btn-primary btn-lg" href="#" role="button">SignUp</a></p>
+          </div>
+
+    </div>
+</div><!-- End of header.row--> 
+
+ <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+
+</html>
+@endsection
